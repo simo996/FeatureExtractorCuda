b59167e6cc403c1fbe8513e5c2422bbcaeeb907c
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-07-16 11:16:03 +0200

    Refactor code structure

--- FeatureExtractor/C++/WindowFeatureComputer.h
+++ FeatureExtractor/C++/WindowFeatureComputer.h
@@ -1,56 +1,27 @@
 //
-// Created by simo on 11/07/18.
+// Created by simo on 16/07/18.
 //
 
-#ifndef FEATUREEXTRACTOR_FEATURECOMPUTER_H
-#define FEATUREEXTRACTOR_FEATURECOMPUTER_H
-
+#ifndef FEATUREEXTRACTOR_WINDOWFEATURECOMPUTER_H
+#define FEATUREEXTRACTOR_WINDOWFEATURECOMPUTER_H
 
 #include <vector>
-#include "GLCM.h"
+#include <map>
 
-enum FeatureList { ASM, AUTOCCORELATION, ENTROPY, MAXPROB, HOMOGENEITY, CONTRAST, DISSIMILARITY, IDM, CORRELATION,
-        CLUSTERPROMINENCE, CLUSTERSHADE, SUMOFSQUARES, SUMAVERAGE, SUMENTROPY, SUMVARIANCE, DIFFENTROPY, DIFFVARIANCE
-};
-
+using namespace std;
 
 class WindowFeatureComputer {
-public:
-    /*
-     * RESPONSABILITA CLASSE: Data la window, generare 1a alla volta le 4 glcm e farci computare le features
+  /*
+     * RESPONSABILITA CLASSE: Computare le feature per la finestra nelle 4 direzioni
      */
-    map<std::string, double> computeFeatures(const GLCM& glcm);
-    void printFeatures(map<std::string, double>& features);
-
-private:
-    double computeASM(const GLCM& glcm);
-    double computeAutocorrelation(const GLCM& glcm);
-    double computeEntropy(const GLCM& glcm);
-    double computeMaximumProbability(const GLCM& glcm);
-    double computeHomogeneity(const GLCM& glcm);
-    double computeContrast(const GLCM& glcm);
-    double computeDissimilarity(const  GLCM& metaGLCM);
-    double computeInverceDifferentMoment(const GLCM& glcm);
-
-    double computeCorrelation(const GLCM& glcm, double muX, double muY, double sigmaX, double sigmaY);
-    double computeClusterProminence(const GLCM& glcm, double muX, double muY);
-    double computeClusterShade(const GLCM& glcm, double muX, double muY);
-    double computeSumOfSquares(const GLCM& glcm, double mu);
-
-    double computeSumAverage(const map<AggregatedGrayPair, int>& summedMetaGLCM, int numberOfPairs);
-    double computeSumEntropy(const map<AggregatedGrayPair, int>& summedMetaGLCM, int numberOfPairs);
-    double computeSumVariance(const map<AggregatedGrayPair, int>& summedMetaGLCM, double sumEntropy, int numberOfPairs);
-
-    double computeDifferenceEntropy(const map<AggregatedGrayPair, int>& aggregatedMetaGLCM, int numberOfPairs);
-    double computeDifferenceVariance(const map<AggregatedGrayPair, int>& aggregatedMetaGLCM, int numberOfPairs);
-
-    double computeMean(const GLCM& glcm);
-    double computeMuX(const GLCM& glcm);
-    double computeMuY(const GLCM& glcm);
-
-    double computeSigmaX(const GLCM& glcm, double muX);
-    double computeSigmaY(const GLCM& glcm, double muY);
+    public:
+        WindowFeatureComputer(vector<int>& inputPixels);
+        vector<map<string, double>> computeFeatures(); // 1 of each of the 4 dimensions
+        void printFeaturesList(vector<map<string, double>>& featureList);
+    private:
+        int distance;
+        int windowDimension;
 };
 
 
-#endif //FEATUREEXTRACTOR_FEATURECOMPUTER_H
+#endif //FEATUREEXTRACTOR_WINDOWFEATURECOMPUTER_H
