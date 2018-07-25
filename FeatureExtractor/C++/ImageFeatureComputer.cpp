0f854afc7d4b0cf8356fc485bd7d3a75d1818afc
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-07-25 13:27:47 +0200

    Introduce image feature computer class

---
+++ FeatureExtractor/C++/ImageFeatureComputer.cpp
@@ -0,0 +1,5 @@
+//
+// Created by simo on 25/07/18.
+//
+
+#include "ImageFeatureComputer.h"
---
+++ FeatureExtractor/C++/ImageFeatureComputer.h
@@ -0,0 +1,18 @@
+/*
+	Created by simo on 25/07/18.
+    * RESPONSABILITA CLASSE: Scorrere l'immagine creando tutte le possibili finestre sovrapposte
+*/
+
+#ifndef FEATUREEXTRACTOR_IMAGEFEATURECOMPUTER_H
+#define FEATUREEXTRACTOR_IMAGEFEATURECOMPUTER_H
+
+
+class ImageFeatureComputer {
+public:
+
+private:
+	
+};
+
+
+#endif //FEATUREEXTRACTOR_IMAGEFEATURECOMPUTER_H
