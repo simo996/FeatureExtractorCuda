9f1f4318fd9b69f8369c60b787ed81766d2caf8d
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-08-01 15:07:37 +0200

    Add methods for saving results to file; add methods to generate feature-images

--- FeatureExtractor/C++/ImageFeatureComputer.cpp
+++ FeatureExtractor/C++/ImageFeatureComputer.cpp
@@ -3,6 +3,8 @@
 //
 
 #include <iostream>
+#include <fstream>
+#include <sys/stat.h>
 #include "ImageFeatureComputer.h"
 
 ImageFeatureComputer::ImageFeatureComputer(const Image& img, const Window& wd)
@@ -38,15 +40,13 @@ vector<WindowFeatures> ImageFeatureComputer::computeAllFeatures(const int number
 
 
 /*
- * This method will generate a vector of maps of features names and all their values found in the image
- * The vector will have an entry for each computed direction
+ * This method will generate a vector of maps (1 for each direction) of features names and all their values found in the image
  * Es. <Entropy , (0.1, 0.2, 3, 4 , ...)>
  * Es. <IMOC, (-1,-2,0)>
  */
 vector<map<FeatureNames, vector<double>>> ImageFeatureComputer::getAllDirectionsAllFeatureValues(const vector<WindowFeatures>& imageFeatures){
 	vector<FeatureNames> supportedFeatures = Features::getAllSupportedFeatures();
 	vector<map<FeatureNames, vector<double>>> output(numberOfDirections);
-	// Desume the number of computed direction by looking at how many elements are stored per window
 
 	// for each computed direction
 	for (int j = 0; j < numberOfDirections; ++j) {
@@ -67,6 +67,54 @@ vector<map<FeatureNames, vector<double>>> ImageFeatureComputer::getAllDirections
 	return output;
 }
 
+void ImageFeatureComputer::saveFeaturesToFiles(const vector<map<FeatureNames, vector<double>>>& imageFeatures){
+	string foldersPath[] ={ "Values0/", "Values45/", "Values90/", "Values135/"};
+	for (int i = 0; i < numberOfDirections; ++i) {
+		saveDirectedFeaturesToFiles(imageFeatures[i], foldersPath[i]);
+	}
+}
+
+void ImageFeatureComputer::saveDirectedFeaturesToFiles(const map<FeatureNames, vector<double>>& imageDirectedFeatures, const string outputFolderPath){
+	typedef map<FeatureNames, vector<double>>::const_iterator MI;
+	vector<string> fileDestinations = Features::getAllFeaturesFileNames();
+
+	int i = 0;
+	for(MI featurePairs = imageDirectedFeatures.begin(); featurePairs != imageDirectedFeatures.end(); featurePairs++){
+		if (mkdir(outputFolderPath.c_str(), 0777) == -1) {
+			if (errno == EEXIST) {
+				// alredy exists
+			} else {
+				// something else
+				cout << "cannot create save folder;  error:" << strerror(errno) << endl;
+			}
+		}
+		string newFileName(outputFolderPath);
+		saveFeatureToFile(*featurePairs, newFileName.append(fileDestinations[i]));
+		i++;
+	}
+}
+
+void ImageFeatureComputer::saveFeatureToFile(const pair<FeatureNames, vector<double>>& featurePair, string filePath){
+	// Open the file
+	ofstream file;
+	file.open(filePath.append(".txt"));
+	if(file.is_open()){
+		for(int i = 0; i < featurePair.second.size(); i++){
+			file << featurePair.second[i] << ",";
+		}
+		file.close();
+	} else{
+		cerr << "Couldn't save the feature values to file" << endl;
+	}
+
+}
+
+/*
+ * This method will print the given vector of maps (1 for each direction)
+ * of features names and all their values found in the image
+ * Es. <Entropy , (0.1, 0.2, 3, 4 , ...)>
+ * Es. <IMOC, (-1,-2,0)>
+ */
 void ImageFeatureComputer::printAllDirectionsAllFeatureValues(const vector<map<FeatureNames, vector<double>>>& featureList)
 {
 	typedef map<FeatureNames, vector<double>>::const_iterator MI;
@@ -86,73 +134,91 @@ void ImageFeatureComputer::printAllDirectionsAllFeatureValues(const vector<map<F
 	}
 }
 
-
 /*
- * This method will print ALL the features, for all the windows of the image,
- * in all 4 directions
- */
-void ImageFeatureComputer::printImageAllDirectionsAllFeatures(const vector<WindowFeatures>& imageFeatureList){
-    typedef vector<WindowFeatures>::const_iterator VI;
+ * This method will create ALL the images associated with each feature,
+ * for ALL the dimension evaluated.
+*/
+void ImageFeatureComputer::saveAllFeatureImages(const vector<map<FeatureNames, vector<double>>> &imageFeatures){
+	string foldersPath[] ={ "Images0/", "Images45/", "Images90/", "Images135/"};
 
-    for (VI windowElement = imageFeatureList.begin(); windowElement!= imageFeatureList.end() ; windowElement++)
-    {
-        WindowFeatureComputer::printAllDirectionsAllFeatures(*windowElement);
-    }
+	// For each direction
+	for(int i=0; i < imageFeatures.size(); i++){
+		saveAllFeatureDirectedImages(imageFeatures[i], foldersPath[i]);
+	}
 }
 
-
 /*
- * This method will print 1 feature, for all the windows of the image,
- * in all 4 directions
- */
-void ImageFeatureComputer::printImageAllDirectionsSingleFeature(const vector<WindowFeatures>& imageFeatureList,
-																FeatureNames fname){
-	typedef vector<WindowFeatures>::const_iterator VI;
-    // For each computed window
-	for (VI windowElement = imageFeatureList.begin(); windowElement!= imageFeatureList.end() ; windowElement++)
-	{
-		WindowFeatureComputer::printAllDirectionsSingleFeature(*windowElement, fname);
+ * This method will create ALL the images associated with each feature,
+ * for 1 dimension evaluated.
+*/
+void ImageFeatureComputer::saveAllFeatureDirectedImages(const map<FeatureNames, vector<double>> &imageDirectedFeatures,
+														string outputFolderPath){
+	typedef map<FeatureNames, vector<double>>::const_iterator MI;
+
+	vector<string> fileDestinations = Features::getAllFeaturesFileNames();
+	int i = 0;
+
+	for(MI feature = imageDirectedFeatures.begin(); feature != imageDirectedFeatures.end(); feature++){
+		saveFeatureImage(imageDirectedFeatures, feature->first,
+						 outputFolderPath.append(fileDestinations[i]));
+		i++;
+	}
+}
+
+void saveImageToFile(const cv::Mat& img, const string fileName){
+	try {
+		imwrite(fileName, img);
+	}catch (exception& e){
+		cout << e.what() << '\n';
+		cerr << "Fatal Error! Couldn't save the image";
+		exit(-3);
 	}
 }
 
 /*
  * This method will create an image associated with a feature,
- * for each dimension evaluated; vector[0] = image of 0°, etc.
+ * for a single dimension evaluated;
 */
-vector<cv::Mat> ImageFeatureComputer::generateFeatureImage(const vector<WindowFeatures> imageFeatures, FeatureNames fname){
+void ImageFeatureComputer::saveFeatureImage(
+		const map<FeatureNames, vector<double>> &imageDirectedFeatures, FeatureNames fname, string filePath){
 	typedef vector<WindowFeatures>::const_iterator VI;
 
+	// Compute how many features will be used for creating the image
 	int numberOfRows = image.getRows() / windowData.distance;
 	int numberOfColumns = image.getColumns() / windowData.distance;
+	int imageSize = numberOfRows * numberOfColumns;
 
-	for(int i = 0; i <imageFeatures.size(); i++){
-
+	// Check if dimensions are compatible
+	if(imageDirectedFeatures.at(fname).size() != imageSize){
+		cerr << "Fatal Error! Couldn't create the image";
+		exit(-2);
 	}
+
+	// Create a 2d matrix of double elements
+	cv::Mat imageFeature = cv::Mat(numberOfRows, numberOfColumns, CV_64F);
+	// CHECK se va a capo da solo e non fa tutto su una riga
+	// Copy the values into the image
+	memcpy(imageFeature.data, imageDirectedFeatures.at(fname).data(), imageSize * sizeof(double));
+
+	//cv::imshow
+	saveImageToFile(imageFeature, filePath);
 }
 
 
+
+// TODO this is a debugging method
 /*
- * This method will print ALL feature, for all the windows of the image,
- * in 1 direction: 0°
+ * This method will print ALL 18 the features, for all the windows of the image,
+ * in all 4 directions
  */
-void ImageFeatureComputer::printImageSingleDirectionsAllFeatures(const vector<WindowFeatures>& imageFeatureList){
+void ImageFeatureComputer::printImageAllDirectionsAllFeatures(const vector<WindowFeatures>& imageFeatureList){
 	typedef vector<WindowFeatures>::const_iterator VI;
 
 	for (VI windowElement = imageFeatureList.begin(); windowElement!= imageFeatureList.end() ; windowElement++)
 	{
-		//WindowFeatureComputer::printSingleDirectionAllFeatures(windowElement[0][0]) ;
+		WindowFeatureComputer::printAllDirectionsAllFeatures(*windowElement);
 	}
 }
 
-/*
- * This method will print 1 feature, for all the windows of the image,
- * in 1 direction: 0°
- */
-void ImageFeatureComputer::printImageSingleDirectionsSingleFeature(const vector<WindowFeatures>& imageFeatureList, FeatureNames fname){
-	typedef vector<WindowFeatures>::const_iterator VI;
 
-	for (VI windowElement = imageFeatureList.begin(); windowElement!= imageFeatureList.end() ; windowElement++)
-	{
-		WindowFeatureComputer::printSingleDirectionSingleFeature(windowElement[0][0], fname);
-	}
-}
+
--- FeatureExtractor/C++/ImageFeatureComputer.h
+++ FeatureExtractor/C++/ImageFeatureComputer.h
@@ -18,27 +18,43 @@ public:
 	// Default = 4 = all feautures ; oder 0->45->90->135°
     vector<WindowFeatures> computeAllFeatures(int numberOfDirections = 4);
 
+    // EXTRAPOLATING RESULTS
 	// This method will get all the feature names and all their values computed in the image
 	vector<map<FeatureNames, vector<double>>> getAllDirectionsAllFeatureValues(const vector<WindowFeatures>& imageFeatures);
 	// This method will print all the feature names and all their values computed in the image
 	void printAllDirectionsAllFeatureValues(const vector<map<FeatureNames, vector<double>>>& featureList);
 
+	// SAVING RESULTS ON FILES
+	/* This method will save on different folders, the features computed for the distinct directions */
+	void saveFeaturesToFiles(const vector<map<FeatureNames, vector<double>>>& imageFeatures);
+
+    // IMAGING
+    // This methow will produce and save all the images associated with each feature for each direction
+    void saveAllFeatureImages(const vector<map<FeatureNames, vector<double>>> &imageFeatures);
+
+
 	// DEBUG, not really useful
 	// Method will print, for each direction, for each window, all the features
 	static void printImageAllDirectionsAllFeatures(const vector<WindowFeatures> &imageFeatureList);
-	// Method will print, for each direction, for each window, all the features
-	static void printImageAllDirectionsSingleFeature(const vector<WindowFeatures> &imageFeatureList, FeatureNames fname);
-
-	vector<cv::Mat> generateFeatureImage(const vector<WindowFeatures> imageFeatures, FeatureNames fname);
-	// TODO these methods might be unuseful; the user provides the number of directions it is interested to
-	static void printImageSingleDirectionsAllFeatures(const vector<WindowFeatures> &imageFeatureList);
-	static void printImageSingleDirectionsSingleFeature(const vector<WindowFeatures> &imageFeatureList, FeatureNames fname);
 private:
 	short int numberOfDirections;
 	Image image;
 	// Information to pass to WindowFeatureComputer equal to all generated windows
 	// dimension, distance, symmetric while
 	Window windowData;
+
+	// SUPPORT FILESAVE methods
+	/* This method will save into the given folder, the features computed for the that directions */
+	void saveDirectedFeaturesToFiles(const map<FeatureNames, vector<double>>& imageFeatures, const string path);
+	/* This method will save into the given folder, 1 feature computed for the that directions */
+	void saveFeatureToFile(const pair<FeatureNames, vector<double>>& imageFeatures, const string path);
+
+	// SUPPORT IMAGING methods
+	// This method will produce and save on the filesystem the image associated with a feature in 1 direction
+	void saveFeatureImage(const map<FeatureNames, vector<double>> &imageDirectedFeatures, FeatureNames fname,
+						  string outputFilePath);
+	// This methow will produce and save all the images associated with each feature in 1 direction
+	void saveAllFeatureDirectedImages(const map<FeatureNames, vector<double>> &imageFeatures, const string outputFolderPath);
 
 };
 
