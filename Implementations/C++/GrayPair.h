9bfbf964b81e98b3d74bbeb4051e85c9aafea65c
za996 <s.galimberti32@campus.unimib.it>
2018-08-11 10:48:39 +0200

    Refactor code order

--- Implementations/C++/GrayPair.h
+++ Implementations/C++/GrayPair.h
@@ -7,19 +7,19 @@
 
 
 class GrayPair {
-    public:
-        void printPair() const;
-        unsigned int getGrayLevelI() const;
-        unsigned int getGrayLevelJ() const;
-        explicit GrayPair(unsigned int i, unsigned int j);
+public:
+    explicit GrayPair(unsigned int i, unsigned int j);
+    unsigned int getGrayLevelI() const;
+    unsigned int getGrayLevelJ() const;
+    void printPair() const;
 
-        bool operator<(const GrayPair& other) const{
-            if(grayLevelI != other.getGrayLevelI())
-                return (grayLevelI < other.getGrayLevelI());
-            else
-                return (grayLevelJ < other.getGrayLevelJ());
-        }
-    private:
+    bool operator<(const GrayPair& other) const{
+        if(grayLevelI != other.getGrayLevelI())
+            return (grayLevelI < other.getGrayLevelI());
+        else
+            return (grayLevelJ < other.getGrayLevelJ());
+    }
+private:
     //  TODO versione alternativa con singolo int per entrambi
         unsigned int grayLevelI;
         unsigned int grayLevelJ;
