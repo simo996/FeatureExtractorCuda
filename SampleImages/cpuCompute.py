b8df0a99c98319b1add699d9f85d4a238148d1f0
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-09-16 23:26:54 +0200

    Change compute script to reflect the change to project structure

--- SampleImages/cpuCompute.py
+++ SampleImages/cpuCompute.py
@@ -2,15 +2,16 @@ import subprocess
 
 variables = ['brain1.tiff', 'brain2.tiff', 'prostata.tiff', 'cells.png', 'nyc4k.jpg']
 
-program = '../Implementations/Pre-Cuda/bin/./FeatureExtractor'
+program = '../Implementations/C++/bin/./FeatureExtractor'
 optionWindowSize = '-w'
+optionWindowSizeValue = '3'
 optionSymmtrecity = '-g'
 optionInputFile = '-i'  
 optionSave = ''
 
 for variable in variables:	
     
-    result = subprocess.run([program, '-i', variable, optionWindowSize, '3', optionSave], stdout=subprocess.PIPE)
+    result = subprocess.run([program, '-i', variable, optionWindowSize, optionWindowSizeValue, optionSave], stdout=subprocess.PIPE)
     print(subprocess.list2cmdline(result.args))
 
     output = result.stdout.decode('utf-8').strip()
