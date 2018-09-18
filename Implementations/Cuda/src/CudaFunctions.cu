0ed481e1714bd97d9836e25c377456c975403e34
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-09-18 20:46:57 +0200

    Remove reservation of spaces for local variables for gpu threads

--- Implementations/Cuda/src/CudaFunctions.cu
+++ Implementations/Cuda/src/CudaFunctions.cu
@@ -101,7 +101,7 @@ void incrementGPUHeap(size_t newHeapSize, size_t featureSize, bool verbose){
 	size_t free, total;
 	cudaMemGetInfo(&free,&total);
 	if(verbose)
-		cout << "\tGPU free memory: (MB) " << (free - newHeapSize - featureSize) / 1024/1024 << endl;
+		cout << "\tGPU used memory: (MB) " << (((newHeapSize + featureSize) / 1024) / 1024) << endl;
 }
 
 /* 
@@ -127,9 +127,6 @@ bool checkEnoughWorkingAreaForThreads(int numberOfPairs, int numberOfThreads,
 		( 4 * sizeof(AggregatedGrayPair) + 1 * sizeof(GrayPair));
 	// Multiply per threads number
 	size_t totalWorkAreas = workAreaSpace * numberOfThreads;
-	// add some space needed for local variables
-	int FACTOR = 1.5; 
-	totalWorkAreas *= FACTOR;
 
 	long long int difference = gpuMemory - (featureSize + totalWorkAreas);
 	if(difference <= 0){
@@ -152,10 +149,7 @@ dim3 getGridFromAvailableMemory(int numberOfPairs,
 
 	// Compute the memory needed for a single thread
 	size_t workAreaSpace = numberOfPairs * 
-		( 4 * sizeof(AggregatedGrayPair) + 1 * sizeof(GrayPair));
-	// add some space needed for local variables
-	int FACTOR = 2; 
-	workAreaSpace *= FACTOR;
+		( 4 * sizeof(AggregatedGrayPair) + 1 * sizeof(GrayPair));	
 
 	// how many thread fit into a single block
 	int threadInBlock = getCudaBlockSideX() * getCudaBlockSideX();
