4c550dcba2a21447697734e8df9f678afc68c226
Simone Galimberti <s.galimberti32@campus.unimib.it>
2018-11-25 18:49:52 +0100

    Update installOpencv.sh

--- installOpencv.sh
+++ installOpencv.sh
@@ -22,9 +22,9 @@ OPENCV_VERSION='3.4.2'
 # 1. KEEP UBUNTU OR DEBIAN UP TO DATE
 
 sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
-sudo apt-get -y autoremove
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+sudo apt-get autoremove
 
 
 # 2. INSTALL THE DEPENDENCIES
